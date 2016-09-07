b49e60103a004f38da4dc35bc3f57a2b913e437c
Thomas Keel <sndbytgkv1@verizon.net>
2016-09-06 21:23:21 -0400

    .

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,17 @@ Here is where you make the connection to the database and export and used by the
 var mysql      = require('mysql');
 var connectionConfig;
 
-if (process.env.JAWSDB_URL){
-  connectionConfig = process.env.JAWSDB_URL;
-}  else {
-    connectionConfig = {
-      port: 3306,
-      host: 'localhost',
-      user: 'root',
-      password: 'tom',
-     database: 'burgers_db'
-   }
-}
+// if (process.env.JAWSDB_URL){
+//   connectionConfig = process.env.JAWSDB_URL;
+// }  else {
+//     connectionConfig = {
+//       port: 3306,
+//       host: 'localhost',
+//       user: 'root',
+//       password: 'tom',
+//      database: 'burgers_db'
+//    }
+// }
 
 //if (process.env.NODE_ENV){
 // connectionConfig = process.env.JAWSDB_URL;
@@ -30,15 +30,16 @@ if (process.env.JAWSDB_URL){
 //  }
 //}
 
-var connection = mysql.createConnection(connectionConfig);
+// var connection = mysql.createConnection(connectionConfig);
+var connection = {};
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('MySQL error connecting: ' + err.stack);
-    return;
-  };
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('MySQL error connecting: ' + err.stack);
+//     return;
+//   };
 
-  console.log('MySQL connected as id ' + connection.threadId);
-})
+//   console.log('MySQL connected as id ' + connection.threadId);
+// })
 
 module.exports = connection;
