cd00c9fdae1654cd953607d9b597318ff83edad4
Thomas Keel <sndbytgkv1@verizon.net>
2016-09-06 21:29:06 -0400

    Updated db config file

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,38 @@
 /*
 Here is where you make the connection to the database and export and used by the O.R.M.
-*/
-// 
-// mysql database
-var mysql      = require('mysql');
-var connectionConfig;
+// */
+// // 
+// // mysql database
+// var mysql      = require('mysql');
+// var connectionConfig;
 
 // if (process.env.JAWSDB_URL){
 //   connectionConfig = process.env.JAWSDB_URL;
 // }  else {
-//     connectionConfig = {
-//       port: 3306,
-//       host: 'localhost',
-//       user: 'root',
-//       password: 'tom',
+//    connectionConfig = {
+//      port: 3306,
+//      host: 'localhost',
+//      user: 'root',
+//      password: 'tom',
 //      database: 'burgers_db'
-//    }
+// //  }
 // }
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection({
+     port: 3306,
+     host: 'localhost',
+     user: 'root',
+     password: 'tom',
+     database: 'burgers_db'
+   })
+}
+
+
 
 //if (process.env.NODE_ENV){
 // connectionConfig = process.env.JAWSDB_URL;
@@ -31,15 +47,14 @@ var connectionConfig;
 //}
 
 // var connection = mysql.createConnection(connectionConfig);
-var connection = {};
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('MySQL error connecting: ' + err.stack);
-//     return;
-//   };
+connection.connect(function(err) {
+  if (err) {
+    console.error('MySQL error connecting: ' + err.stack);
+    return;
+  };
 
-//   console.log('MySQL connected as id ' + connection.threadId);
-// })
+  console.log('MySQL connected as id ' + connection.threadId);
+})
 
 module.exports = connection;
